40b02e1533efff20d6c1aa1dff6a3157f8320126
Diana Nekrasova <ya.nel2014@ya.ru>
2021-10-20 20:47:03 +0300

    Подключен слайдер Slick

---
+++ js/scriptSlick.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        arrows: false,
+        dots: false,
+        touchThreshold: 8,
+        infinite: true,
+        variableWidth: true,
+    });
+});
