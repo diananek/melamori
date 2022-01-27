a02bc31cf0d4514fe0ee141ea81577769021af6b
Diana Nekrasova <ya.nel2014@ya.ru>
2022-01-27 20:52:18 +0300

    Добавлена главная страница

--- melamori-next-app/pages/index.js
+++ melamori-next-app/pages/index.js
@@ -1,8 +1,19 @@
+import MainLayout from "../components/MainLayout";
+import Offers from "../components/Offers";
+import StoreSections from "../components/StoreSections";
+import Catalog from "../components/Catalog";
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+          <MainLayout>
+              <div className="container">
+                  <div className="page__logo">
+                      <img src="img/logo.svg" alt="Логотип Me Lamori"/>
+                  </div>
+              </div>
+              <Offers/>
+              <StoreSections/>
+              <Catalog title="Популярное"/>
+          </MainLayout>
   )
 }
