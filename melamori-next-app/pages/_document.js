5657b8eca7aa1c180d1b2fe9141101022f803de9
Diana Nekrasova <ya.nel2014@ya.ru>
2022-01-31 12:12:35 +0300

    Добавлены мета теги и тег title

--- melamori-next-app/pages/_document.js
+++ melamori-next-app/pages/_document.js
@@ -10,6 +10,10 @@ export default class MyDocument extends Document {
         return (
             <Html lang="ru">
                 <Head>
+                    <title>MeLamori</title>
+                    <meta charSet="UTF-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&display=swap"
