cbbed1ad7b2befe5e3070207b1964455ae75573b
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-07 16:11:32 +0300

    Добавлена функция получения ключей для запросов в зависимости от категории

---
+++ melamori-next-app/lib/getKeys.js
@@ -0,0 +1,25 @@
+export function getKeys(category) {
+    let keys = {
+        prices: '',
+        sizeRelation: '',
+        size: '',
+    }
+    switch (category) {
+        case 'bed_collection':
+            keys.prices = 'bed_prices_id'
+            keys.sizeRelation = 'bed_size_relation'
+            keys.size = 'bed_size'
+            break;
+        case 'sofa_collection':
+            keys.prices = 'sofa_prices_id'
+            keys.sizeRelation = 'sofa_size_relation'
+            keys.size = 'sofa_size'
+            break;
+        case 'mattresses':
+            keys.prices = 'mattresses_prices_id'
+            keys.sizeRelation = 'mattress_size_relation'
+            keys.size = null
+            break;
+    }
+    return keys;
+}
