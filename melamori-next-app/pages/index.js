7e610f6756c785f3ba4fbd38876d321c0ea79b41
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-07 16:02:14 +0300

    Добавлен запрос коллекции кроватей

--- melamori-next-app/pages/index.js
+++ melamori-next-app/pages/index.js
@@ -4,18 +4,31 @@ import StoreSections from "../components/StoreSections";
 import Catalog from "../components/Catalog";
 import Image from "next/image";
 import logo from "../public/img/logo.svg"
+import {useQuery} from "@apollo/client";
+import {bedCollection} from "../graphql/queries";
+import {Script} from "next/script";
 
 export default function Home() {
+    const {loading, error, data} = useQuery(bedCollection)
+    let status;
+    if(loading) {
+        status = "loading"
+    }
+    if (error) {
+        status = "error"
+    }
   return (
           <MainLayout>
               <div className="container">
                   <div className="page__logo">
-                      <Image src={logo} alt="Логотип Me Lamori" layout={"fill"}/>
+                      <Image src={logo} alt="Логотип MeLamori" layout={"fill"}/>
                   </div>
               </div>
               <Offers/>
               <StoreSections/>
-              <Catalog title="Популярное"/>
+              <Catalog title="Популярное" productsData={data} status={status}
+                       collectionName="bed_collection"/>
+
           </MainLayout>
   )
 }
