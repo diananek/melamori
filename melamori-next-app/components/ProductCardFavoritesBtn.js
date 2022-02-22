6154c4ff4fa5a5085d54f140f64738e5cc952af3
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-22 13:57:42 +0300

    Добавлен функционал для избранных товаров
    При нажатии на кнопку товар добавляется в куки

--- melamori-next-app/components/ProductCardFavoritesBtn.js
+++ melamori-next-app/components/ProductCardFavoritesBtn.js
@@ -1,13 +1,38 @@
 import {useState} from "react";
+import Cookie from "js-cookie";
+import {useAppContext} from "../context/state";
 
-export default function ProductCardFavoritesBtn(props) {
-    let [isPressed, setPressed] = useState(false);
+export default function ProductCardFavoritesBtn({className, id}) {
+    const ctx = useAppContext();
+
+    let [isPressed, setPressed] = useState(ctx.cookieFav.includes(id) );
 
     let classNames = require('classnames');
-    let btnClass = classNames("product-card__fav", props.className, {
+    let btnClass = classNames("product-card__fav", className, {
         "product-card__fav_active": isPressed
     })
+    if(Cookie.get('site_consent')) {
+        ctx.cookieFav.includes(id) ?? setPressed(true)
+    }
+    const removeId = ()=> {
+        const index = ctx.cookieFav.indexOf(id, 0)
+        ctx.setCookieFav(ctx.cookieFav.slice(0, index).concat(ctx.cookieFav.slice(index+1)))
+    }
+
     return(
-        <button className={btnClass} onClick={()=>{setPressed(!isPressed)}}> </button>
+        <button className={btnClass} onClick={()=>{
+            const isActive = !isPressed
+            setPressed(isActive)
+            console.log(ctx.cookieFav)
+            if(isActive) {
+                if(!ctx.cookieFav.includes(id)){
+                    ctx.setCookieFav(ctx.cookieFav.concat(id))
+                }
+            } else {
+                removeId()
+            }
+
+        }}> </button>
     )
 }
+
