99d0ec285dbe6b4583db57693b0e7ac2ff5290f3
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-23 17:35:58 +0300

    Добавлен функционал для корзины аналогичный избранным товарам

--- melamori-next-app/context/state.js
+++ melamori-next-app/context/state.js
@@ -5,20 +5,46 @@ const AppContext = createContext();
 
 export function AppWrapper({ children}) {
     let [cookieFav, setCookieFav] = useState(undefined)
+    let [cookieBasket, setCookieBasket] = useState(undefined)
 
     useEffect(()=> {
-        console.log(cookieFav)
-        if (cookieFav !== undefined) {
-            Cookie.set('favorites', JSON.stringify(cookieFav),{
-                expires: 30
-            })
-        } else {
-            setCookieFav(JSON.parse(Cookie.get('favorites')))
+        if(Cookie.get('site_consent')) {
+            if(Cookie.get('favorites') === undefined) {
+                Cookie.set('favorites', JSON.stringify([]),{
+                    expires: 30
+                })
+            }
+            if (cookieFav !== undefined) {
+                Cookie.set('favorites', JSON.stringify(cookieFav),{
+                    expires: 30
+                })
+            } else {
+                setCookieFav(JSON.parse(Cookie.get('favorites')))
+            }
         }
+
     }, [cookieFav])
+    useEffect(()=> {
+        console.log(cookieBasket)
+        if(Cookie.get('site_consent')) {
+            if(Cookie.get('basket') === undefined) {
+                Cookie.set('basket', JSON.stringify([]),{
+                    expires: 30
+                })
+            }
+            if (cookieBasket !== undefined) {
+                Cookie.set('basket', JSON.stringify(cookieBasket),{
+                    expires: 30
+                })
+            } else {
+                setCookieBasket(JSON.parse(Cookie.get('basket')))
+            }
+        }
+
+    }, [cookieBasket])
 
     return (
-        <AppContext.Provider value={{cookieFav, setCookieFav}}>
+        <AppContext.Provider value={{cookieFav, setCookieFav, cookieBasket, setCookieBasket}}>
             {children}
         </AppContext.Provider>
     );
