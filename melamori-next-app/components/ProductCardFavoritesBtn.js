7a5632c9c8790de9edafe8575ff2a3a6a7019959
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-23 18:20:53 +0300

    Добавлено условие для проверки наличия куков

--- melamori-next-app/components/ProductCardFavoritesBtn.js
+++ melamori-next-app/components/ProductCardFavoritesBtn.js
@@ -1,38 +1,50 @@
 import {useState} from "react";
-import Cookie from "js-cookie";
 import {useAppContext} from "../context/state";
+import classNames from "classnames";
 
 export default function ProductCardFavoritesBtn({className, id}) {
     const ctx = useAppContext();
-
-    let [isPressed, setPressed] = useState(ctx.cookieFav.includes(id) );
-
     let classNames = require('classnames');
-    let btnClass = classNames("product-card__fav", className, {
-        "product-card__fav_active": isPressed
-    })
-    if(Cookie.get('site_consent')) {
+    if(ctx.cookieFav) {
+        let [isPressed, setPressed] = useState(ctx.cookieFav.includes(id) );
+
+        let btnClass = classNames("product-card__fav", className, {
+            "product-card__fav_active": isPressed
+        })
+
         ctx.cookieFav.includes(id) ?? setPressed(true)
-    }
-    const removeId = ()=> {
-        const index = ctx.cookieFav.indexOf(id, 0)
-        ctx.setCookieFav(ctx.cookieFav.slice(0, index).concat(ctx.cookieFav.slice(index+1)))
-    }
 
-    return(
-        <button className={btnClass} onClick={()=>{
-            const isActive = !isPressed
-            setPressed(isActive)
-            console.log(ctx.cookieFav)
-            if(isActive) {
-                if(!ctx.cookieFav.includes(id)){
-                    ctx.setCookieFav(ctx.cookieFav.concat(id))
+        const removeId = ()=> {
+            const index = ctx.cookieFav.indexOf(id, 0)
+            ctx.setCookieFav(ctx.cookieFav.slice(0, index).concat(ctx.cookieFav.slice(index+1)))
+        }
+
+        return(
+            <button className={btnClass} onClick={()=>{
+                const isActive = !isPressed
+                setPressed(isActive)
+                if(isActive) {
+                    if(!ctx.cookieFav.includes(id)){
+                        ctx.setCookieFav(ctx.cookieFav.concat(id))
+                    }
+                } else {
+                    removeId()
                 }
-            } else {
-                removeId()
-            }
 
-        }}> </button>
-    )
+            }}> </button>
+        )
+    } else {
+        let [isPressed, setPressed] = useState(false);
+
+        let btnClass = classNames("product-card__fav", className, {
+            "product-card__fav_active": isPressed
+        })
+        return(
+            <button className={btnClass} onClick={()=>{
+                setPressed(!isPressed)
+            }}> </button>
+        )
+    }
+
 }
 
--- melamori-next-app/components/ProductFavoritesBtn.js
+++ melamori-next-app/components/ProductFavoritesBtn.js
@@ -1,13 +1,48 @@
 import {useState} from "react";
+import {useAppContext} from "../context/state";
+import classNames from "classnames";
 
-export default function ProductFavoritesBtn(props) {
-    let [isPressed, setPressed] = useState(false);
-
+export default function ProductFavoritesBtn({className, id}) {
     let classNames = require('classnames');
-    let btnClass = classNames("product__favorites", props.className, {
-        "product__favorites_active": isPressed
-    })
-    return(
-        <button className={btnClass} onClick={()=>{setPressed(!isPressed)}}> <span> </span> </button>
-    )
+
+    const ctx = useAppContext();
+    if(ctx.cookieFav) {
+        let [isPressed, setPressed] = useState(ctx.cookieFav.includes(id) );
+
+        let btnClass = classNames("product__favorites", className, {
+            "product__favorites_active": isPressed
+        })
+
+        const removeId = ()=> {
+            const index = ctx.cookieFav.indexOf(id, 0)
+            ctx.setCookieFav(ctx.cookieFav.slice(0, index).concat(ctx.cookieFav.slice(index+1)))
+        }
+
+        return(
+            <button className={btnClass} onClick={()=>{
+                const isActive = !isPressed
+                setPressed(isActive)
+                console.log(ctx.cookieFav)
+                if(isActive) {
+                    if(!ctx.cookieFav.includes(id)){
+                        ctx.setCookieFav(ctx.cookieFav.concat(id))
+                    }
+                } else {
+                    removeId()
+                }
+
+            }}> <span> </span> </button>
+        )
+    } else {
+        let [isPressed, setPressed] = useState(false);
+
+        let btnClass = classNames("product__favorites", className, {
+            "product__favorites_active": isPressed
+        })
+        return(
+            <button className={btnClass} onClick={()=>{
+                setPressed(!isPressed)
+            }}> <span> </span> </button>
+        )
+    }
 }
