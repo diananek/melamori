29bf476064ce563543ddc81180675452b8ff451c
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-23 18:12:07 +0300

    Добавлены плашки с кол-вом товаров в избранных и корзине

--- melamori-next-app/components/Header.js
+++ melamori-next-app/components/Header.js
@@ -2,8 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "../public/img/logo.svg";
 import Menu from "./Menu";
+import {useAppContext} from "../context/state";
 
 export default function Header() {
+    const ctx = useAppContext()
+    const basketCount = ctx.cookieBasket !== undefined ? ctx.cookieBasket.length : 0
+    const favoritesCount = ctx.cookieFav !== undefined ? ctx.cookieFav.length : 0
     return(
         <header className="header header_catalog">
             <div className="header__body container">
@@ -15,10 +19,12 @@ export default function Header() {
                 </Link>
                 <Link href={"/favorites"}>
                     <a className="header__favorites header__favorites_catalog">
+                        {favoritesCount !== 0 ? <span>{favoritesCount}</span>: undefined}
                     </a>
                 </Link>
                 <Link href={"/basket"}>
                     <a className="header__basket header__basket_catalog">
+                        {basketCount !== 0 ? <span>{basketCount}</span>: undefined}
                     </a>
                 </Link>
             </div>
