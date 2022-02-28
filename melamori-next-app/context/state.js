c6e2d6b4a027f917c4245893db3995a5c0ff5810
Diana Nekrasova <ya.nel2014@ya.ru>
2022-02-28 10:35:39 +0300

    Изменено название старых переменных и добавлены новые состояния
    В соответствии с этих изменены и названия куков

--- melamori-next-app/context/state.js
+++ melamori-next-app/context/state.js
@@ -3,37 +3,46 @@ import Cookie from "js-cookie";
 
 const AppContext = createContext();
 
-export function AppWrapper({ children}) {
-    let [cookieFav, setCookieFav] = useState(undefined)
-    let [cookieBasket, setCookieBasket] = useState(undefined)
+export function AppWrapper({children}) {
+    const [cookieFavId, setCookieFavId] = useState(undefined)
+    const [cookieFav, setCookieFav] = useState([])
+    const [cookieBasket, setCookieBasket] = useState(undefined)
 
-    useEffect(()=> {
-        if(Cookie.get('site_consent')) {
-            if(Cookie.get('favorites') === undefined) {
-                Cookie.set('favorites', JSON.stringify([]),{
+    useEffect(() => {
+        if (Cookie.get('site_consent')) {
+            console.log(cookieFav)
+            if (Cookie.get('favorites_id') === undefined) {
+                Cookie.set('favorites_id', JSON.stringify([]), {
+                    expires: 30
+                })
+                Cookie.set('favorites', JSON.stringify([]), {
                     expires: 30
                 })
             }
-            if (cookieFav !== undefined) {
-                Cookie.set('favorites', JSON.stringify(cookieFav),{
+            if (cookieFavId !== undefined) {
+                Cookie.set('favorites_id', JSON.stringify(cookieFavId), {
+                    expires: 30
+                })
+                Cookie.set('favorites', JSON.stringify(cookieFav), {
                     expires: 30
                 })
             } else {
+                setCookieFavId(JSON.parse(Cookie.get('favorites_id')))
                 setCookieFav(JSON.parse(Cookie.get('favorites')))
             }
         }
 
-    }, [cookieFav])
-    useEffect(()=> {
+    }, [cookieFavId, cookieFav])
+    useEffect(() => {
         console.log(cookieBasket)
-        if(Cookie.get('site_consent')) {
-            if(Cookie.get('basket') === undefined) {
-                Cookie.set('basket', JSON.stringify([]),{
+        if (Cookie.get('site_consent')) {
+            if (Cookie.get('basket') === undefined) {
+                Cookie.set('basket', JSON.stringify([]), {
                     expires: 30
                 })
             }
             if (cookieBasket !== undefined) {
-                Cookie.set('basket', JSON.stringify(cookieBasket),{
+                Cookie.set('basket', JSON.stringify(cookieBasket), {
                     expires: 30
                 })
             } else {
@@ -44,7 +53,7 @@ export function AppWrapper({ children}) {
     }, [cookieBasket])
 
     return (
-        <AppContext.Provider value={{cookieFav, setCookieFav, cookieBasket, setCookieBasket}}>
+        <AppContext.Provider value={{cookieFavId, setCookieFavId, setCookieFav, cookieFav, cookieBasket, setCookieBasket}}>
             {children}
         </AppContext.Provider>
     );
