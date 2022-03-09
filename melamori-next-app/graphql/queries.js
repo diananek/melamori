e8d747af0a948a8f8170a13d55bfcc37ddd60784
Diana Nekrasova <ya.nel2014@ya.ru>
2022-03-09 17:37:54 +0300

    Изменены запросы

--- melamori-next-app/graphql/queries.js
+++ melamori-next-app/graphql/queries.js
@@ -1,7 +1,17 @@
 import {gql} from "@apollo/client";
 
-export const bedCollection = gql`query myQuery($offset: Int){
-       bed_collection (offset: $offset){
+export const bedCollection = gql`query myQuery($offset: Int, $limit: Int){
+       bed_collection (offset: $offset, limit: $limit){
+           additional_options {
+               id
+               additional_options_id {
+                   id
+                   percentage
+                   price
+                   title
+                   description
+               }
+           }
            image {
       id
       title
@@ -10,12 +20,17 @@ export const bedCollection = gql`query myQuery($offset: Int){
     id
     price_list {
       bed_prices_id {
-        price
-        status
+          id
+          price
+          status
           sale_percentage
           bed_size_relation {
               bed_size
               sleep_size
+          }
+          bed_cloth_category_relation {
+              category
+              id
           }
       }
     }
@@ -23,8 +38,8 @@ export const bedCollection = gql`query myQuery($offset: Int){
     }
     `
 
-export const sofaCollection = gql`{
-    sofa_collection {
+export const sofaCollection = gql`query myQuery($offset: Int, $limit: Int){
+    sofa_collection (offset: $offset, limit: $limit){
         image {
             id
             title
@@ -79,8 +94,10 @@ export const bedCollectionById = gql`query myQuery($id:ID!){
             bed_prices_id {
                 bed_cloth_category_relation {
                     category
+                    id
                 }
                 bed_size_relation {
+                    id
                     sleep_size
                     bed_size
                 }
@@ -92,6 +109,59 @@ export const bedCollectionById = gql`query myQuery($id:ID!){
         additional_options {
             additional_options_id {
                 title
+                description
+            }
+        }
+    }
+}
+`
+export const sofaCollectionById = gql`query myQuery($id:ID!){
+    sofa_collection_by_id(id: $id) {
+        title
+        image {
+            id
+            title
+        }
+        price_list {
+            sofa_prices_id {
+                sofa_cloth_category_relation {
+                    category
+                }
+                sofa_size_relation {
+                    sleep_size
+                    sofa_size
+                }
+                price
+                status
+                sale_percentage
+            }
+        }
+        additional_options {
+            additional_options_id {
+                title
+                description
+            }
+        }
+    }
+}
+`
+
+export const mattressCollectionById = gql`query myQuery($id:ID!){
+    mattresses_by_id(id: $id) {
+        image {
+            id
+            title
+        }
+        id
+        title
+        price_list {
+            mattresses_prices_id {
+                price
+                status
+                sale_percentage
+                mattress_size_relation {
+                    sleep_size
+                }
             }
         }
     }
