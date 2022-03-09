58e05faafdf420a664bf58c12c522bc3e44b9c24
Diana Nekrasova <ya.nel2014@ya.ru>
2022-03-09 17:23:28 +0300

    Добавлены доп. ключи

--- melamori-next-app/lib/getKeys.js
+++ melamori-next-app/lib/getKeys.js
@@ -8,16 +8,19 @@ export function getKeys(category) {
         case 'bed_collection':
             keys.prices = 'bed_prices_id'
             keys.sizeRelation = 'bed_size_relation'
+            keys.clothRelation = 'bed_cloth_category_relation'
             keys.size = 'bed_size'
             break;
         case 'sofa_collection':
             keys.prices = 'sofa_prices_id'
             keys.sizeRelation = 'sofa_size_relation'
+            keys.clothRelation = 'sofa_cloth_category_relation'
             keys.size = 'sofa_size'
             break;
         case 'mattresses':
             keys.prices = 'mattresses_prices_id'
             keys.sizeRelation = 'mattress_size_relation'
+            keys.clothRelation = 'mattresses_cloth_category_relation'
             keys.size = null
             break;
     }
