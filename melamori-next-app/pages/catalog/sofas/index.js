3be4c95648f3118b846ec8fb2059afa4a2f5b09b
Diana Nekrasova <ya.nel2014@ya.ru>
2022-03-22 15:12:52 +0300

    Изменено название запроса и коллекции

--- melamori-next-app/pages/catalog/sofas/index.js
+++ melamori-next-app/pages/catalog/sofas/index.js
@@ -1,13 +1,13 @@
 import {useRouter} from "next/router";
 import {getPageData} from "../../../lib/getPageData";
 import {useQuery} from "@apollo/client";
-import {sofaCollection} from "../../../graphql/queries";
+import {softFurnitureCollection} from "../../../graphql/queries";
 import CatalogPage from "../../../components/CatalogPage";
 
 export default function Sofas() {
     const pathname = useRouter().asPath
     const title = getPageData(pathname)
-    const {loading, error, data} = useQuery(sofaCollection)
+    const {loading, error, data} = useQuery(softFurnitureCollection)
 
     let status;
 
@@ -19,6 +19,6 @@ export default function Sofas() {
     }
 
     return(
-        <CatalogPage title={title} collectionName={'sofa_collection'} status={status} data={data}/>
+        <CatalogPage title={title} collectionName={'soft_furniture'} status={status} data={data}/>
     )
 }
