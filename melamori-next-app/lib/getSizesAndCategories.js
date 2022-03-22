eb78b01746eb9ffbb1158513ebe8aa80be86a2d4
Diana Nekrasova <ya.nel2014@ya.ru>
2022-03-22 15:15:40 +0300

    Удалены цены и изменен тип данных для sizes и clothCategories

--- melamori-next-app/lib/getSizesAndCategories.js
+++ melamori-next-app/lib/getSizesAndCategories.js
@@ -1,16 +1,14 @@
 export function getSizesAndCategories(data, keys) {
-    let sizes = new Map();
-    let clothCategories = new Map();
-    let prices = new Map();
+    let sizes = [];
+    let clothCategories = [];
     data.price_list.forEach((p)=>{
         let price = p[keys.prices];
-        let clothCategory =price[keys.clothRelation];
+        let clothCategory = price[keys.clothRelation];
         let size = price[keys.sizeRelation];
-        sizes.get(clothCategory) ? sizes.set(clothCategory, [...sizes.get(clothCategory), size]): sizes.set(clothCategory, [size]);
-        clothCategories.get(size) ? clothCategories.set(size, [...clothCategories.get(size), clothCategory]):
-            clothCategories.set(size, [clothCategory]);
-        prices.set([clothCategory, size], price);
+
+        !sizes.includes(size) ? sizes.push(size) : undefined;
+        !clothCategories.includes(clothCategory) ? clothCategories.push(clothCategory) : undefined;
     })
 
-    return [sizes, clothCategories, prices]
+    return [sizes, clothCategories]
 }
