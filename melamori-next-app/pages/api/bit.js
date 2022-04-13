15a0fd4d492018b121bd9c2fbd9b22f8d08caae8
rest <shkerdin.v@yandex.ru>
2022-04-13 18:28:20 +0300

    fixes

--- melamori-next-app/pages/api/bit.js
+++ melamori-next-app/pages/api/bit.js
@@ -1,14 +1,15 @@
 import Bitrix from '@2bad/bitrix'
 
-const bitrix = Bitrix('https://b24-z9pkrw.bitrix24.ru/rest/88/ehstc0lz7lk4kmdp/')
+const bitrix = Bitrix('https://melamori.bitrix24.ru/rest/88/ehstc0lz7lk4kmdp/')
+// const bitrix = Bitrix('https://b24-z9pkrw.bitrix24.ru/rest/88/ehstc0lz7lk4kmdp/')
 
 
 export default function handler(req, res) {
-
+    console.log(req.body)
 
     if (req.headers['api-key-he-he'] === '0413a78e-ec00-47c3-8112-bb39ae3c5402') {
 
-        debugger
+        // debugger
         bitrix.leads.create({
             TITLE: 'Заказ с сайта',
             HAS_PHONE: "Y",
